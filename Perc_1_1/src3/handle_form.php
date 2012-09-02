d0ef5c430758db82929d626789011ffa9baba6b8
Ken Haduch <khaduch@gmail.com>
2012-09-01 23:30:04 -0400

    Add files from chapter 3 of the book.
    Nothing added for the Review and Pursue section at this time

---
+++ Perc_1_1/src3/handle_form.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title>Your Feedback</title>
+</head>
+<body>
+
+<?php	// Script 3.3 - handle_form.php
+
+// This page receives the data from feedback.html
+// it will receive: title, name, email, response, comments, and submit in $_POST
+
+ini_set('display_errors', 1);  // let me learn from my mistakes!
+error_reporting(E_ALL | E_STRICT);  // show all possible problems
+
+// print_r($_POST);
+
+// Create shorthand versions of the variables
+$title = $_POST['title'];
+$name = $_POST['name'];
+$response = $_POST['response'];
+$comments = $_POST['comments'];
+
+// Print the received data
+print "<p>Thank you, $title $name, for your comments.</p>
+<p>You stated that you found this example to be '$response' and added:<br />$comments</p>";
+
+?>
+</body>
+</html>
